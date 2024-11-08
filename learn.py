96a83fc3dfa01b6a67d0743a0a86bd3a54080f09
Temake <130784362+Temake@users.noreply.github.com>
2024-11-08 02:16:54 +0100

    v

--- learn.py
+++ learn.py
@@ -1,5 +1,4 @@
-# Author: Ardit Sulce, Automate Everything with Python, Udemy
-# Course URL: https://www.udemy.com/course/automate-everything-with-python/
+
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -21,36 +20,12 @@ def get_drvier():
 
 def main():
   driver = get_drvier()
-  driver.find_element(by="id", value="CustomerEmail").send_keys("app7flask@gmail.com")
+  driver.find_element(by="id", value="CustomerEmail").send_keys("temedia005@gmailcom" )
   time.sleep(2)
-  driver.find_element(by="id", value="CustomerPassword").send_keys("??!65EhGMg8.WwY" + Keys.RETURN)
+  driver.find_element(by="id", value="CustomerPassword").send_keys("temini2006" + Keys.RETURN)
   time.sleep(2)
   driver.find_element(by="xpath", value='//*[@id="shopify-section-footer"]/section/div/div[1]/div[1]/div[1]/nav/ul/li[1]/a').click()
   print(driver.current_url)
 
 print(main())
 
-import requests
-from datetime import datetime
-import time
-
-ticker = input("Enter the ticker symbol: ")
-from_date = input('Enter start date in yyyy/mm/dd format:')
-to_date = input('Enter end date in yyyy/mm/dd format:')
-
-from_datetime = datetime.strptime(from_date, '%Y/%m/%d')
-to_datetime = datetime.strptime(to_date, '%Y/%m/%d')
-
-from_epoch = int(time.mktime(from_datetime.timetuple()))
-to_epoch = int(time.mktime(to_datetime.timetuple()))
-
-
-url = f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={from_epoch}&period2={to_epoch}&interval=1d&events=history&includeAdjustedClose=true"
-
-headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"} 
-
-content = requests.get(url, headers=headers).content
-print(content)
-
-with open('data.csv', 'wb') as file:
-  file.write(content)
